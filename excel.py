5842bf7b7c63c8645a78185a828fdc1214dbe093
CoffeePoweredComputers <davidsmith@hamiltonfour.tech>
2021-10-07 13:58:25 -0500

    updated

---
+++ excel.py
@@ -0,0 +1,61 @@
+def sum(num_list):
+    '''
+    Implement the sum function using the total/sum pattern
+    '''
+
+def stdev(num_list):
+    '''
+    Implement the standard deviation of a list of numbers
+    '''
+
+def avg(num_list):
+    '''
+    Implement
+    '''
+
+def get_rows(x):
+    '''
+    Given a list of strings convert to list of lists
+    where each sublist is a list of integers
+    '''
+
+def get_cols(x):
+    '''
+    Given a list of strings convert to list of lists
+    where each sublist is a list of integers from the 
+    columns of the original csv. (e.g., for a csv with
+    three columns, you should have three sublist)
+    '''
+
+
+def plot(num_lists):
+    '''
+    Plots the data from the *columns* of the file.
+    '''
+
+def write_data(outfile, data, axis):
+    '''
+    Args:
+        outfile -> The file the user wants to write to.
+        data    -> The data the user wants to write to the file.
+        axis    -> Indicates if sublists are columns (0) or rows (1)
+    '''
+    if axis == 1:
+        continue #Note: Replace this with code that will write to rows
+    else:
+        continue #Note: Replace this with code that will write to columns
+
+
+if __name__ == "__main__":
+
+    # 1.1) Get first argument, the csv file path/name, from the user
+
+    # 1.2) Get second argument, the operation from the user
+
+    # 1.3) Get third argument, the csv path/file to write to
+
+    # 2) Check to see if it's a valid operation
+
+    # 3) If it is, perform it and write the results to a file
+
+
