1dc7e53c2b64e05654de2240a2f54709c066e392
CoffeePoweredComputers <davidsmith@hamiltonfour.tech>
2021-10-13 16:50:57 -0500

    updated

--- excel.py
+++ excel.py
@@ -1,61 +1,41 @@
-def sum(num_list):
-    '''
-    Implement the sum function using the total/sum pattern
-    '''
+import os
+import sys
 
-def stdev(num_list):
-    '''
-    Implement the standard deviation of a list of numbers
-    '''
-
-def avg(num_list):
-    '''
-    Implement
-    '''
-
-def get_rows(x):
-    '''
-    Given a list of strings convert to list of lists
-    where each sublist is a list of integers
-    '''
-
-def get_cols(x):
-    '''
-    Given a list of strings convert to list of lists
-    where each sublist is a list of integers from the 
-    columns of the original csv. (e.g., for a csv with
-    three columns, you should have three sublist)
-    '''
+import excelmath as em
 
 
-def plot(num_lists):
-    '''
-    Plots the data from the *columns* of the file.
-    '''
+def main():
 
-def write_data(outfile, data, axis):
-    '''
-    Args:
-        outfile -> The file the user wants to write to.
-        data    -> The data the user wants to write to the file.
-        axis    -> Indicates if sublists are columns (0) or rows (1)
-    '''
-    if axis == 1:
-        continue #Note: Replace this with code that will write to rows
+    #
+    # Part 1: processing the users arguments
+    #
+
+    if len(sys.argv) < 4:
+        print("Invalid number of arguments")
+        exit(-1)
+
+    if os.path.exists(sys.argv[1]) and os.path.isfile(sys.argv[1]) and sys.argv[1].endswith(".csv"):
+        with open(sys.argv[1]) as fo:
+            raw_data = fo.readlines()
     else:
-        continue #Note: Replace this with code that will write to columns
+        print("{} invalid path".format(sys.argv[1]))
+        exit(-2)
+
+    if sys.argv[2] == "row":
+        processed_data = em.get_rows(raw_data)
+    elif sys.argv[2] == "col":
+        processed_data = em.get_cols(raw_data)
+    else:
+        print("Please enter either col or row")
+        exit(-3)
+
+    operations = ["sum", "stdev", "avg"]
+    op_funcs = [em.sum, em.stdev, em.avg]
+    for operation in sys.argv[2:]:
+        if 
+
 
 
 if __name__ == "__main__":
-
-    # 1.1) Get first argument, the csv file path/name, from the user
-
-    # 1.2) Get second argument, the operation from the user
-
-    # 1.3) Get third argument, the csv path/file to write to
-
-    # 2) Check to see if it's a valid operation
-
-    # 3) If it is, perform it and write the results to a file
-
+    main()
 
---
+++ src/excelmath.py
@@ -0,0 +1,29 @@
+def sum(num_list):
+    '''
+    Implement the sum function using the total/sum pattern
+    '''
+
+def stdev(num_list):
+    '''
+    Implement the standard deviation of a list of numbers
+    '''
+
+def avg(num_list):
+    '''
+    Implement
+    '''
+
+def get_rows(x):
+    '''
+    Given a list of strings convert to list of lists
+    where each sublist is a list of integers
+    '''
+
+def get_cols(x):
+    '''
+    Given a list of strings convert to list of lists
+    where each sublist is a list of integers from the 
+    columns of the original csv. (e.g., for a csv with
+    three columns, you should have three sublist)
+    '''
+
